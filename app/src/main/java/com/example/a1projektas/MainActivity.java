68be6f08750be5664c48ac3082d61af519b7f8bd
Miglevi <Visockaite.migle@gmail.com>
2024-03-03 15:53:28 +0200

    Revert "Added change text and the second button"
    This reverts commit 567e265d382cb5b7e6f16231c5e45a6d77ecbc56.

--- app/src/main/java/com/example/a1projektas/MainActivity.java
+++ app/src/main/java/com/example/a1projektas/MainActivity.java
@@ -3,27 +3,12 @@ package com.example.a1projektas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.tvMain = findViewById(R.id.tvMain);
-    }
-
-    public void btnTextClick(View view) {
-        this.tvMain.setText("Hello");
-    }
-
-    public void changecolor(View view) {
-        this.tvMain.setTextColor(Color.BLUE);
     }
 }
